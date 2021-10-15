e84098a406bf2d8d370999cdc2634270c3c5b0e5
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-10-15 09:51:05 -0300

    Usando condição diferentes  em JS.

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Vivendo em ${país}.`)
+if (país == 'Brasil') { // Pode ser usado o sinal de != (Não igual)
+  console.log('Você é Brasileiro!')
+} else {
+  console.log('Você é estrangeiro')
+}
