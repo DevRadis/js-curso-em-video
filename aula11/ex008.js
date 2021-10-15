25e1b8972e9aa1d59d84ecff2fe66e5fd7fb0f9c
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-10-15 09:43:26 -0300

    Usando condição simples em JS.

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 60.2
+console.log(`O seu carro está a ${vel}Km/h`)
+if (vel > 60) { // condição simples
+  console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
