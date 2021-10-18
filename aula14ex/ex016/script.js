248389f616a8250dee250020d2672bd042720272
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-10-17 22:48:20 -0300

    Aprendendo na prática, botando a mão na massa.

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,20 +1,20 @@
 function contar() {
-  var ini = window.document.querySelector('div#txti')
-  var fim = window.document.querySelector('div#txtf')
-  var passo = window.document.querySelector('div#txtp')
+  let ini = window.document.querySelector('div#txti')
+  let fim = window.document.querySelector('div#txtf')
+  let passo = window.document.querySelector('div#txtp')
   
   if (txti.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0) {
     window.alert('[ERRO] Verifique os dados e tente novamente!')
     //res.innerHTML = 'Impossível contar!'
   } else {
-    window.alert('TUDO OK!')
-    /*
-    var ini = Number(txtin.value)
-    var fim = Number(txtfim.value)
-    var pass = Number(txtpas.value)
-    if ((ini.value + fim.value) * pass == res) {
-      res.innerHTML += `${res} Este é o resultado`
-    }*/
+    res.innerHTML = 'Contando: '
+    let i = Number(ini.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+    
+    for(let c = ) 
+      
+    }
   }
 }
 
