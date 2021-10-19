a0502396babd368b1888e9ea3041cfd65207aad1
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-10-18 22:40:09 -0300

    Finalizado primeiro projeto de conta! :smile:

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -2,16 +2,31 @@ function contar() {
   let ini = window.document.querySelector('div#txti')
   let fim = window.document.querySelector('div#txtf')
   let passo = window.document.querySelector('div#txtp')
+  let res = window.document.querySelector('div#res')
   
   if (txti.value.length == 0 || txtf.value.length == 0 || txtp.value.length == 0) {
-    window.alert('[ERRO] Verifique os dados e tente novamente!')
-    //res.innerHTML = 'Impossível contar!'
+    res.innerHTML = 'Impossível contar!'
+    window.alert('[ERRO] Faltam dados!')
   } else {
-    res.innerHTML = 'Contando: '
-    let i = Number(ini.value)
-    let f = Number(fim.value)
-    let p = Number(passo.value)
-      
+    res.innerHTML = 'Contando: <br>'
+    let i = Number(txti.value)
+    let f = Number(txtf.value)
+    let p = Number(txtp.value)
+    if (p <= 0) {
+      window.alert('Passo inválido! Será considerado PASSO 1')
+      p = 1
+    }
+    if (i < f) {
+      // Contagem crescente
+      for(let c = i; c <= f; c+= p) {
+        res.innerHTML += ` ${c} \u{1F449} `
+      }
+    } else {
+      // Contagem regressiva
+      for(let c = i; c >= f; c -= p) {
+        res.innerHTML += ` ${c} \u{1F449}`
+      }
+    }
+      res.innerHTML += `\u{1F3C1}`
     }
   }
-
