3f99a5ef4db277142785947f14b08a761b282c9d
Paulo Radis <paulo_saldanha90@hotmail.com>
2021-10-19 19:36:38 -0300

    Finalizado projeto Calculadora!

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,1 +1,18 @@
-// alert('Olá')
+function tabuada() {
+  let num = window.document.getElementById('txtn')
+  let tab = window.document.getElementById('seltab')
+  
+  if (num.value.length == 0) {
+    window.alert('Por favor digite um número!')
+  } else {
+    let n = Number(num.value)
+    let c = 1
+    tab.innerHTML = '' // colocar para limpar a tabuada quando digitar uma
+    while (c <= 10) {
+      let item = window.document.createElement('option')
+      item.text = `${n} x ${c} = ${n*c}`
+      tab.appendChild(item)
+      c++
+    }
+  }
+}
